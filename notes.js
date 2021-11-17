e4d9e52d4b2c5077f9fdbfee77bb3b01821d5e01
DougSilvera <shortxjdoug@gmail.com>
2021-11-17 12:43:25 -0500

    initial commit notes list

---
+++ notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        id:1,
+        subject: "github",
+        date: "11/15/2021",
+        feeling: "cried vigorously",
+        timeSpent: 35
+    },
+    {
+        id: 2,
+        subject: "html",
+        date: "11/12/2021",
+        feeling: "fear and loathing",
+        timeSpent: 45
+    },
+    {
+        id: 3,
+        subject: "css",
+        date: "11/11/2021",
+        feeling: "despair",
+        timeSpent: 24
+    }
+]
+console.log(notes);
